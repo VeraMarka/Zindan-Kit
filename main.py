7162297605a1fed43f40ce4ecf64c26e5ea29f2f
VeraMarka <165566710+VeraMarka@users.noreply.github.com>
2024-04-17 23:15:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
-
-# Renkli metin oluşturmak için ANSI kaçış dizilerini kullan
+import os 
+os.system("clear")
+os.system("figlet Zindan")
 class colors:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
@@ -9,24 +10,20 @@ class colors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-# Kullanılabilir araçları ve renklerini bir sözlükte tanımla
 tools = {
     "1": {"name": "izlenme.py", "color": colors.BLUE},
     "2": {"name": "ipconfig.py", "color": colors.GREEN},
     "3": {"name": "port.py", "color": colors.BLUE},
     "4": {"name": "videoindirme.py", "color": colors.GREEN},
-		"5": {"name: "webip.py", "color": colors.GREEN},
+    "5": {"name": "webip.py", "color": colors.GREEN},
 }
 
-# Menüyü göster
 print("Kullanılabilir araçlar:")
 for key, tool in tools.items():
     print(tool["color"] + key + ". " + tool["name"] + colors.ENDC)
 
-# Kullanıcıdan seçim yapmasını iste
-selection = input("Hangi aracı çalıştırmak istiyorsunuz? (1/2/3/4/5): ",colors.HEADER)
+selection = input("Hangi aracı çalıştırmak istiyorsunuz? (1/2/3/4/5): ")
 
-# Kullanıcının seçimine göre ilgili aracı çalıştır
 if selection in tools:
     tool_name = tools[selection]["name"]
     subprocess.run(["python", tool_name])
